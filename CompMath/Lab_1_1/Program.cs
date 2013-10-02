eb3fa4c655148ed009e4c3633fbd1cef24c4d8fe
Stas Frolov <ratatui4ik@gmail.com>
2013-10-02 14:47:56 +0300

    Init

---
+++ CompMath/Lab_1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Library.Set;
+namespace Lab_1_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Set<int> set = new Set<int>();
+		}
+	}
+}
---
+++ CompMath/Library/Set.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace Library.Set
+{
+	public class Set<T> : System.Collections.IEnumerable
+	{
+		private sealed class Node
+		{
+			public T Data;
+			public Node Next;
+
+			public Node(T data, Node next)
+			{
+				this.Data = data;
+				this.Next = next;
+			}
+		}
+
+		public System.Collections.IEnumerator GetEnumerator()
+		{
+			for (Node currentNode = head; currentNode != null; currentNode = currentNode.Next)
+				yield return currentNode.Data;
+		}
+
+		private int count;
+		public int Count
+		{
+			get { return this.count; }
+		}
+
+		private Node head = null;
+		private Node tail = null;
+
+		public void Insert(int position, T data)
+		{
+			if (position < 0 || position > this.count)
+				throw new ArgumentOutOfRangeException();
+
+			if (position == 0)
+				this.AddFront(data);
+			else if (position == this.count)
+				this.AddBack(data);
+			else
+			{
+				Node curentNode = this.head;
+				for (int i = 0; i < this.count; i++)
+					curentNode = curentNode.Next;
+
+				Node newNode = new Node(data, curentNode.Next);
+				curentNode.Next = newNode;
+
+				this.count++;
+			}
+		}
+
+		public void AddFront(T data)
+		{
+			if (this.head == null)
+			{
+				this.head = new Node(data, null);
+				this.tail = this.head;
+			}
+			else
+			{
+				Node newHead = new Node(data, this.head);
+				this.head = newHead;
+			}
+			this.count++;
+		}
+
+		public void AddBack(T data)
+		{
+			if (this.head == null)
+			{
+				this.head = new Node(data, null);
+				this.tail = this.head;
+			}
+			else
+			{
+				this.tail.Next = new Node(data, null);
+				this.tail = this.tail.Next;
+			}
+			this.count++;
+		}
+
+		public void Print()
+		{
+			System.Console.Write("Set:");
+			foreach (var nodeValue in this)
+			{
+				System.Console.Write(nodeValue + " ");
+			}
+			System.Console.WriteLine();
+		}
+
+		public void Sort()
+		{
+
+		}
+	}
+}
